5797452ab1d5c5a7f4fefa03cf1ad351231f7e36
Sorachat Patanasombat <jingjingmaple@gmail.com>
2017-10-25 21:07:10 +0700

    finnish map

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import arcade
 from pprint import pprint
-SPRITE_SCALING = 0.15
+SPRITE_SCALING = 0.13
 
-SCREEN_WIDTH = 1280
+SCREEN_WIDTH = 1080
 SCREEN_HEIGHT = 720
 # Physics
 
@@ -34,65 +34,183 @@ class WorldWindow(arcade.Window):
 		self.all_sprites_list.update()
 
 	def on_key_press(self, key, modifiers):
-		'''if key == arcade.key.UP:
-			self.player_sprite.change_y = MOVEMENT_SPEED
-		elif key == arcade.key.DOWN:
-			self.player_sprite.change_y = -MOVEMENT_SPEED'''
-		#elif
+		
 		'''if key == arcade.key.UP:
 			if self.physics_engine.can_jump():
 				self.player_sprite.change_y = JUMP_SPEED
-		el'''
-		if key == arcade.key.LEFT:
+		'''
+		if key == arcade.key.UP:
+			self.player_sprite.change_y = MOVEMENT_SPEED
+		elif key == arcade.key.DOWN:
+			self.player_sprite.change_y = -MOVEMENT_SPEED
+	
+		
+		elif key == arcade.key.LEFT:
 			self.player_sprite.change_x = -MOVEMENT_SPEED
 		elif key == arcade.key.RIGHT:
 			self.player_sprite.change_x = MOVEMENT_SPEED
 	def on_key_release(self, key, modifiers):
  
-		#if key == arcade.key.UP or key == arcade.key.DOWN:
-			#self.player_sprite.change_y = 0
-		#elif
-		if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+		if key == arcade.key.UP or key == arcade.key.DOWN:
+			self.player_sprite.change_y = 0
+		elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
 			self.player_sprite.change_x = 0
 	def setup(self):
 
 		self.all_sprites_list = arcade.SpriteList()
 		self.wall_list = arcade.SpriteList()
 		self.stair_list = arcade.SpriteList()
+		self.gate_list = arcade.SpriteList()
 
 		self.score = 0
-		self.player_sprite = Player(stair_list)
+		self.player_sprite = Player()
 		self.player_sprite.center_x = 900
-		self.player_sprite.center_y = 279
+		self.player_sprite.center_y = 110
 		#self.player_sprite.center_y = SCREEN_HEIGHT / 2
 		self.all_sprites_list.append(self.player_sprite)
+		
 
-		for x in range(173, 650, 64):
+
+		#base floor
+		for x in range(32, 1280, 64):
+			wall = arcade.Sprite("images/PNG/Ground/Grass/grassMid.png", 0.5) #128 to 64x64
+			wall.center_x = x
+			wall.center_y = 32
+			self.all_sprites_list.append(wall)
+			self.wall_list.append(wall)
+		
+		#gate
+			#base floor
+			stair = arcade.Sprite("images/gate.png",0.5)
+			stair.center_x = 153
+			stair.center_y = 124
+			self.all_sprites_list.append(stair)
+			self.stair_list.append(stair)
+
+			#second floor
+			stair = arcade.Sprite("images/gate.png",0.5)
+			stair.center_x = 52
+			stair.center_y = 272
+			self.all_sprites_list.append(stair)
+			self.stair_list.append(stair)
+
+			#third floor
+			stair = arcade.Sprite("images/gate.png",0.5)
+			stair.center_x = 125
+			stair.center_y = 420
+			self.all_sprites_list.append(stair)
+			self.stair_list.append(stair)
+
+			#fourth floor
+			stair = arcade.Sprite("images/gate.png",0.5)
+			stair.center_x = 52
+			stair.center_y = 568
+			self.all_sprites_list.append(stair)
+			self.stair_list.append(stair)
+
+		#upper pedan
+		for x in range(32, 1280, 64):
+			wall = arcade.Sprite("images/PNG/Ground/Grass/grassCenter.png", 0.5)
+			wall.center_x = x
+			wall.center_y = 688
+			self.all_sprites_list.append(wall)
+			self.wall_list.append(wall)
+		'''#left wall
+		for y in range(64, 720, 64):
+			wall = arcade.Sprite("images/PNG/Ground/Stone/stoneCenter.png", 0.5)
+			wall.center_x = 32
+			wall.center_y = y
+			self.all_sprites_list.append(wall)
+			self.wall_list.append(wall)'''
+
+		#first floor
+		#-----------------------------------------------------------
+		stair = arcade.Sprite("images/stair3.png",0.8)
+		stair.center_x = 408
+		stair.center_y = 140
+		self.all_sprites_list.append(stair)
+		self.stair_list.append(stair)
+
+		for x in range(500, 700, 32):
+			wall = arcade.Sprite("images/brick.png", 0.5) #64x64 to 32x32
+			wall.center_x = x
+			wall.center_y = 200
+			self.all_sprites_list.append(wall)
+			self.wall_list.append(wall)
+
+		stair = arcade.Sprite("images/stair2.png",0.8)
+		stair.center_x = 784
+		stair.center_y = 140
+		self.all_sprites_list.append(stair)
+		self.stair_list.append(stair)
+		#-----------------------------------------
+		stair = arcade.Sprite("images/stair3.png",0.8)
+		stair.center_x = 308
+		stair.center_y = 288
+		self.all_sprites_list.append(stair)
+		self.stair_list.append(stair)
+		for x in range(0, 250, 32):
+			wall = arcade.Sprite("images/brick.png", 0.5) #64x64 to 32x32
+			wall.center_x = x
+			wall.center_y = 200
+			self.all_sprites_list.append(wall)
+			self.wall_list.append(wall)
+		
+		#second floor
+
+		for x in range(0, 164, 32):
+			wall = arcade.Sprite("images/brick.png", 0.5) #32x32
+			wall.center_x = x
+			wall.center_y = 348
+			self.all_sprites_list.append(wall)
+			self.wall_list.append(wall)
+
+		for x in range(400, 960, 32):
+			wall = arcade.Sprite("images/brick.png", 0.5) #32x32
+			wall.center_x = x
+			wall.center_y = 348
+			self.all_sprites_list.append(wall)
+			self.wall_list.append(wall)
+
+		#third floor
+		for x in range(0, 392, 32):
+			wall = arcade.Sprite("images/brick.png", 0.5) #32x32
+			wall.center_x = x
+			wall.center_y = 500
+			self.all_sprites_list.append(wall)
+			self.wall_list.append(wall)
+
+		stair = arcade.Sprite("images/stair3.png",0.8)
+		stair.center_x = 628
+		stair.center_y = 440
+		self.all_sprites_list.append(stair)
+		self.stair_list.append(stair)
+
+		for x in range(720, 960, 32):
+			wall = arcade.Sprite("images/brick.png", 0.5) #32x32
+			wall.center_x = x
+			wall.center_y = 500
+			self.all_sprites_list.append(wall)
+			self.wall_list.append(wall)
+		
+
+		
+
+		'''for x in range(173, 650, 64):
 			wall = arcade.Sprite("images/floor.png", 1)
 			wall.center_x = x
 			wall.center_y = 200
 			self.all_sprites_list.append(wall)
 			self.wall_list.append(wall)
-			print(x)
+			print(x)'''
 
-		for x in range(0, 170, 64):
-			wall = arcade.Sprite("images/floor.png", 0.6)
-			wall.center_x = x
-			wall.center_y = 400
-			self.all_sprites_list.append(wall)
-			self.wall_list.append(wall)
 
-		stair = arcade.Sprite("images/stair.png",0.7)
-		stair.center_x = 570
-		stair.center_y = 320
-		self.all_sprites_list.append(stair)
-		self.stair_list.append(stair)
 		'''self.physics_engine = \
 			arcade.PhysicsEnginePlatformer(self.player_sprite,
 										   self.wall_list,
 										   gravity_constant=GRAVITY)'''
-		print(len(self.wall_list))
-		print(self.wall_list[10].height)
+		'''print(len(self.wall_list))
+		print(self.wall_list[10].height)'''
 	def stair(x_left,y_right):
 		for _stair in self.stair_list:
 			if x_left < (_stair.center_x+(_stair.width/2)):
